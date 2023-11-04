a5e3e3c8c818c81dafa64254c1befb4d895e0379
Ravenyzled <131689575+SnowySunnyDaze@users.noreply.github.com>
2023-11-05 00:04:14 +0900

    - Update Data

--- database/Data.sql
+++ database/Data.sql
@@ -3,3 +3,7 @@
 INSERT INTO CuaHang 
 VALUES ('AMShop', 'Cửa Hàng Quần Áo - AMShop', '12 Nguyễn Văn Bảo, Phường 4, Quận Gò Vấp, Thành phố Hồ Chí Minh', '0339998822')
 
+INSERT INTO CaLamViec 
+VALUES 
+(1, 'Ca Sáng', '08:00', '15:00'),
+(2, 'Ca Chiều', '15:00', '22:00')
