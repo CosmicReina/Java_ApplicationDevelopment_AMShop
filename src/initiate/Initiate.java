0a4fcf79e5eb48e65c3e08896faa015b586a0cd3
Ravenyzled <131689575+SnowySunnyDaze@users.noreply.github.com>
2023-11-07 22:26:34 +0900

    - Create FrmMain & PnlMain

--- src/initiate/Initiate.java
+++ src/initiate/Initiate.java
@@ -1,12 +1,14 @@
 package initiate;
 
 import connect.ConnectDB;
+import gui.FrmMain;
 
 public class Initiate {
     public static void main(String[] args) {
         ConnectDB.getInstance().connectDatabase();
         
-        
+        FrmMain frmMain = FrmMain.getInstance();
+        frmMain.setVisible(true);
         
         ConnectDB.getInstance().disconnectDatabase();
     }
