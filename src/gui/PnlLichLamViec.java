a39e046a7bc5e53da7554044d7de8f69347ac784
NguyenHuuPhuocHai <hain35284@gmail.com>
2023-11-07 12:01:34 +0700

    - Update & Create

---
+++ src/gui/PnlLichLamViec.java
@@ -0,0 +1,18 @@
+package gui;
+
+public class PnlLichLamViec extends javax.swing.JPanel {
+    public PnlLichLamViec() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setLayout(new java.awt.BorderLayout());
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+
+}
