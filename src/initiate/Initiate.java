14677655e4f87c42ab1eec3572ce63afe05cb52e
Ravenyzled <131689575+SnowySunnyDaze@users.noreply.github.com>
2023-11-07 22:01:53 +0900

    - Update Initiate && Create UtilityJButton

--- src/initiate/Initiate.java
+++ src/initiate/Initiate.java
@@ -1,7 +1,13 @@
 package initiate;
 
+import connect.ConnectDB;
+
 public class Initiate {
     public static void main(String[] args) {
+        ConnectDB.getInstance().connectDatabase();
         
+        
+        
+        ConnectDB.getInstance().disconnectDatabase();
     }
 }
